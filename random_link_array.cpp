74ce0916684a1c18effaf9dfd187e576b3dd4ff3
zhengwei <zhengwei@weizmann-PC.(none)>
2014-06-11 09:47:35 +0800

    fix bugs

--- random_link_array.cpp
+++ random_link_array.cpp
@@ -17,11 +17,12 @@ void init_random_pointer(random_node*, int);
 int get_node_value(random_node*);
 random_node* init_random_list(const int list_length) {
     random_node* list = new random_node[list_length];
-    for (int i = 0; i < list_length - 1; i++) {
+    for (int i = 0; i < list_length; i++) {
         list[i].next = &list[i+1];
         list[i].value = i;
     }
     list[list_length - 1].next = NULL;
+    list[list_length - 1].value = list_length - 1;
     init_random_pointer(list, list_length);
     return list;
 }
