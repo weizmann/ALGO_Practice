3e1dfe0cf572a64d2d7460b65e1c77e405d0e8c4
weizmann@github.com <zhengwei0828@126.com>
2015-03-06 23:52:32 +0800

    add artillery, dp with multi-states

---
+++ poj/1185_artillery/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int Graph[];
+int dp[110][65][65];
+
+int main() {
+	int row, col;
+	cin >> row >> col;
+	for (int row_index = 0; row_index < row; row_index++) {
+		for (int col_index = 0; col_index < col; col_index++) {
+			
+		}
+	}
+}
