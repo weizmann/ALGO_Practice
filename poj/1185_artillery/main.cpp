5ead1383b9ee24f4b382976d7a86a4a8b580b8f1
weizmann@github.com <zhengwei0828@126.com>
2015-03-07 14:38:13 +0800

    move some miscellaneous into leetcode/miscellaneous

--- poj/1185_artillery/main.cpp
+++ poj/1185_artillery/main.cpp
@@ -2,15 +2,28 @@
 
 using namespace std;
 
-int Graph[];
+int Graph[110];
 int dp[110][65][65];
 
 int main() {
 	int row, col;
 	cin >> row >> col;
+	char input_char;
+
+	memset(Graph, 0, sizeof(Graph));
+	memset(dp, 0, sizeof(dp));
+
 	for (int row_index = 0; row_index < row; row_index++) {
 		for (int col_index = 0; col_index < col; col_index++) {
-			
+			cin >> input_char;
+			if (input_char == 'P') {
+				Graph[row_index] += (1 << col_index);
+			}
 		}
 	}
+
+	for (int index = 0; index <= (1 << col); index++) {
+		
+	}
+
 }
